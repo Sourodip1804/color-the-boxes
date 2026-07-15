67535ed2c958e3690610359ccc99716d3d2372c8
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-15 12:45:25 +0530

    addind comments to the new.js file

--- new.js
+++ new.js
@@ -1,3 +1,3 @@
 console.log("Script.js initializing...")
-let boxes = document.getElementsByClassName("box") //to get all the boxes in an array
+let boxes = document.getElementsByClassName("box") //to get all the boxes in an array and this is a html collection have no for each method
 console.log(boxes)
