b0f1bcc9d8363d3b3c1d90c97f079210ffa9796b
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-15 12:49:01 +0530

    commenting

--- new.js
+++ new.js
@@ -1,5 +1,10 @@
 console.log("Script.js initializing...")
-let boxes = document.getElementsByClassName("box") //to get all the boxes in an array and this is a html collection have no for each method
+// let boxes = document.getElementsByClassName("box") //to get all the boxes in an array and this is a html collection have no for each method
+
+// this can also be written as 
+
+
+
 console.log(boxes)
 
 Array.from(boxes).forEach((e) => {
